1116a4a42433cce62dfdd5e9ad3acf7e55a35032
Yukimi Kazari <kazari@acr.moe>
2024-10-10 04:21:32 -0400

    Added User based file user researching

--- js/cache.js
+++ js/cache.js
@@ -2193,7 +2193,7 @@ docutrol@acr.moe - 301-399-3671 - docs.acr.moe/docutrol
         const image = await loadImage(imageBuffer);
         const imgRatio = image.width / image.height;
         const canvasRatio = width / height;
-        const crop = (opts && opts.crop && opts.crop.length > 0) ? opts.crop.filter(e => e.type === (canvasRatio <= 1))[0] : undefined;
+        const crop = (opts && opts.crop && opts.crop.length > 0) ? opts.crop.filter(e => e.type === (canvasRatio <= 1) ? 1 : 0)[0] : undefined;
 
         Logger.printLine("ImageGenerator", `Canvas@${canvasRatio.toFixed(2)} Image@${imgRatio.toFixed(2)} with result of ${(canvasRatio - imgRatio).toFixed(4)}: ${((canvasRatio - imgRatio) > 0.5 || (canvasRatio - imgRatio) < -0.5) ? "Generate" : "Direct"} Results`, "info");
 
